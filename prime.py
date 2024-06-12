a002196a31bbedd6827e490349c1dfee51c0e876
salman-srv <veerankisalmanraj9@gmail.com>
2024-06-12 00:45:36 -0400

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,17 @@
+def is_prime(number):
+    if number <= 1: 
+        return False
+    elif number == 2:  
+        return True
+    elif number % 2 == 0: 
+        return False
+    else:
+        for i in range(3, int(number**0.5) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
+number = int(input())
+if is_prime(number):
+    print(True)
+else:
+    print(False)
